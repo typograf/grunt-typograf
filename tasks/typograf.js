9f171127c8383c6e193fcd574c78691e08414d0f
kobezzza <kobezzza@gmail.com>
2015-05-24 18:33:57 +0300

    Refactoring

--- tasks/typograf.js
+++ tasks/typograf.js
@@ -6,11 +6,13 @@
  * https://github.com/typograf/grunt-typograf/blob/master/LICENSE
  */
 
-var Typograf = require('typograf');
+var
+	Typograf = require('typograf');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('typograf', 'Prepare texts with Typograf', function () {
-		var options = this.options(),
+		var
+			options = this.options(),
 			typograf = new Typograf(options);
 
 		if (options.disable) {
--- test/typograf_test.js
+++ test/typograf_test.js
@@ -1,4 +1,6 @@
-var grunt = require('grunt');
+var
+	grunt = require('grunt');
+
 exports.typograf = {
 	setUp: function (done) {
 		done();
@@ -7,21 +9,17 @@ exports.typograf = {
 	test: function (test) {
 		test.expect(3);
 
-		var n = /(?:\r?\n|\r)$/;
-		var input = grunt.file.read('tmp/test.txt'),
+		var
+			input1 = grunt.file.read('tmp/test.txt'),
 			input2 = grunt.file.read('tmp/test2.txt'),
 			input3 = grunt.file.read('tmp/test3.txt');
 
-		var expected = grunt.file.read('test/expected/test.txt')
-			.replace(n, '');
+		var
+			expected1 = grunt.file.read('test/expected/test.txt').trim(),
+			expected2 = grunt.file.read('test/expected/test2.txt').trim(),
+			expected3 = grunt.file.read('test/expected/test3.txt').trim();
 
-		var expected2 = grunt.file.read('test/expected/test2.txt')
-			.replace(n, '');
-
-		var expected3 = grunt.file.read('test/expected/test3.txt')
-			.replace(n, '');
-
-		test.equal(input, expected, 'should prepare text');
+		test.equal(input1, expected1, 'should prepare text');
 		test.equal(input2, expected2, 'should prepare text with disabled rule "common/punctuation/exclamation"');
 		test.equal(input3, expected3, 'should prepare text with enabled rule "ru/money/ruble"');
 
